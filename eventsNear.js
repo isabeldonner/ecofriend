5a84de8e2a46a1b1f85250fddab7a449bf64368a
matilde-gillia <gilamonster.omg@gmail.com>
2025-10-22 10:45:13 -0400

    eventbrite api

--- eventsNear.js
+++ eventsNear.js
@@ -1,6 +1,22 @@
 import * as React from 'react';
 import { View, Text } from 'react-native';
 
+const token = 'IEB3TDJEQOBY44QQ4TMZ';
+const apiUrl = 'https://www.eventbrite.com/oauth/token';
+
+fetch(apiUrl, {
+  headers: {
+    "Authorization": `Bearer ${token}`
+  }
+})
+.then(response => response.json())
+.then(data => 
+  {console.log('User data:', data);
+})
+.catch(error => {
+  console.error('Error fetching user data:', error);
+});
+
 export default function eventsNear() {
   return (
     <View><Text>Events</Text></View>
